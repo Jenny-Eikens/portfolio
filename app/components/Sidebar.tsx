6b7a5e9b879abc0fb43db60bd50930880e70e094
Jenny-Eikens <jennifereikens@web.de>
2025-02-25 18:57:45 +0100

    Reposition sidebar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <>
-      <div className="absolute bottom-0 h-[80px] w-full border-2 border-purple-500 px-4 py-4 md:fixed md:right-0 md:h-full md:w-[80px] md:py-0">
-        <ul className="flex w-full list-none flex-row items-center justify-center space-x-10 border-2 border-blue-500 md:h-full md:w-auto md:flex-col md:space-x-0 md:space-y-10">
+      <div className="h-[80px] w-full px-4 py-4 md:fixed md:right-0 md:h-full md:w-[80px] md:py-0">
+        <ul className="flex w-full list-none flex-row items-center justify-center space-x-10 md:h-full md:w-auto md:flex-col md:space-x-0 md:space-y-10">
           <li>
             <a
               href="https://github.com/Jenny-Eikens"
               target="_blank"
               rel="noreferrer noopener"
             >
-              <div className="group relative flex items-center">
-                <span className="absolute left-0 flex h-[40px] max-w-0 -translate-x-0 items-center overflow-hidden whitespace-nowrap rounded bg-white px-2 py-1 text-sm text-black opacity-0 transition-all duration-1000 ease-in-out group-hover:w-auto group-hover:max-w-[250px] group-hover:-translate-x-full group-hover:opacity-100">
+              <div className="social-container group">
+                <span className="social hidden md:inline">
                   Check out my Github profile!
                 </span>
                 <img
@@ -28,8 +28,8 @@ const Sidebar = () => {
               target="_blank"
               rel="noreferrer noopener"
             >
-              <div className="group relative flex items-center">
-                <span className="absolute left-0 flex h-[40px] max-w-0 -translate-x-0 items-center overflow-hidden whitespace-nowrap rounded bg-white px-2 py-1 text-sm text-black opacity-0 transition-all duration-1000 ease-in-out group-hover:w-auto group-hover:max-w-[250px] group-hover:-translate-x-full group-hover:opacity-100">
+              <div className="social-container group">
+                <span className="social">
                   Check out my Frontend Mentor profile!
                 </span>
                 <img
@@ -42,10 +42,8 @@ const Sidebar = () => {
           </li>
           <li>
             <a href="mailto:jennifereikens@web.de" target="_blank">
-              <div className="group relative flex items-center">
-                <span className="absolute left-0 flex h-[40px] max-w-0 -translate-x-0 items-center overflow-hidden whitespace-nowrap rounded bg-white px-2 py-1 text-sm text-black opacity-0 transition-all duration-1000 ease-in-out group-hover:w-auto group-hover:max-w-[250px] group-hover:-translate-x-full group-hover:opacity-100">
-                  Send me an email!
-                </span>
+              <div className="social-container group">
+                <span className="social">Send me an email!</span>
                 <div className="rounded-full bg-white">
                   <img
                     src="/images/envelope-simple.svg"
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import Sidebar from './components/Sidebar'
 
+{
+  /* TODO
+  - fix sidebar disappearing
+  - find better solution for hover effect
+  - fix background image expanding vertically when there is more content
+  */
+}
+
 const Home = () => {
   return (
     <>
       <div className="relative">
-        <Sidebar />
+        <div className="hidden md:block">
+          <Sidebar />
+        </div>
         <main className="m-auto min-h-[90vh] w-[90vw] max-w-[1100px] space-y-10 pr-20 text-3xl">
           <section className="h-screen">
             <h1 className="text-primary text-4xl font-bold">
@@ -14,33 +24,10 @@ const Home = () => {
               <code className="text-secondary">&lt;/&gt;</code>
             </h1>
           </section>
-          <section>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni modi
-            corporis animi asperiores consectetur fugit sapiente deserunt,
-            maiores sint! Accusantium enim totam eum nobis minus aliquam, animi
-            quibusdam temporibus reprehenderit laudantium error eligendi dolor
-            distinctio ducimus fugit quos, at repellendus quisquam maxime ipsam.
-            Quos nam, quidem deserunt soluta at ipsa eligendi, fugiat assumenda
-            harum pariatur, eum veritatis numquam impedit cum ducimus laborum
-            fugit corporis inventore officiis culpa! Ipsam quo sapiente quidem
-            itaque aliquam aspernatur consectetur non corporis nesciunt deserunt
-            illum quisquam ullam tenetur, provident sit vitae corrupti libero
-            fugit mollitia est, ea iste! Magnam quas sapiente facere illo
-            architecto? Ut expedita maxime saepe aperiam iste at minus ex
-            dignissimos veritatis natus sunt aliquid sint repellat
-            exercitationem ullam reiciendis nulla, facilis porro veniam quis
-            fuga vitae? Molestiae impedit dolorem obcaecati est esse, alias
-            adipisci aliquam asperiores exercitationem voluptatem consectetur
-            libero laudantium? Molestiae eaque reiciendis praesentium ea enim
-            eligendi expedita provident harum incidunt. Ut pariatur dolor
-            nostrum et repellendus blanditiis deleniti iusto consequuntur aut
-            modi. Corporis aliquam quam, natus totam doloremque maxime cum est
-            corrupti excepturi, ex fugiat? Repellendus dolorem recusandae maxime
-            aut temporibus, quod cum nulla asperiores facilis rem fugiat
-            quibusdam. Dolorem reiciendis vero sunt veritatis quo delectus
-            omnis! Culpa, tenetur.
-          </section>
         </main>
+        <div className="md:hidden">
+          <Sidebar />
+        </div>
         {/*<footer className="bottom-1 mt-6 p-2 text-center text-sm md:bottom-0 md:p-0"></footer>*/}
       </div>
     </>
