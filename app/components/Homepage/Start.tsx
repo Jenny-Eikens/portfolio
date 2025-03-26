6eadb1e4769d03b8b0d11d7d4145ca8dc54854d4
Jenny-Eikens <jennifereikens@web.de>
2025-03-26 22:31:52 +0100

    Exchange Typewriter for TypeIt

--- app/components/Homepage/Start.tsx
+++ app/components/Homepage/Start.tsx
@@ -5,11 +5,11 @@ const Start = () => {
   return (
     <>
       <div className="text-center md:mb-6 md:text-left">
-        <code>&lt;&gt;</code>{' '}
+        <code>&lt;&gt;</code>
         <h1 className="inline text-clip bg-gradient-heading bg-clip-text text-[4.5rem] font-bold leading-[5rem] text-transparent md:text-[8rem] md:leading-[7rem]">
           Jennifer <br />
           Eikens
-        </h1>{' '}
+        </h1>
         <code>&lt;/&gt;</code>
       </div>
       <h2 className="text-[3rem] font-bold leading-[3rem] md:text-[4rem]">
